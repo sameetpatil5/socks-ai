a7442e078f6ada14d133de4ee61c06f8021e56cc
Sameet Patil <sameetpatil5@gmail.com>
2025-02-11 15:06:44 +0530

    Fix invalid Stock chart plot, Fix Reset button

--- src/socks_chart.py
+++ src/socks_chart.py
@@ -3,6 +3,7 @@ import yfinance as yf
 from modules.stock_chart_agent import StockChartAgent
 from streamlit_components.st_horizontal import st_horizontal
 import datetime
+import logging
 
 if "plot_type" not in st.session_state:
     st.session_state.plot_type = "none"
@@ -19,7 +20,13 @@ def plot_chart(symbol, period, interval, plot_type, chart_type, indicators):
 
 
 def reset_selections():
-    for key in ["symbol", "time_period", "chart_type", "indicators"]:
+    for key in [
+        "chart_symbol",
+        "chart_time_period",
+        "chart_time_interval",
+        "chart_chart_type",
+        "chart_indicators",
+    ]:
         if key in st.session_state:
             del st.session_state[key]
     st.rerun()
@@ -35,59 +42,56 @@ with st.container(border=True):
         col1, col2, col3, col4, col5 = st.columns([2, 1, 1, 1.5, 4])
 
         with col1:
-            symbol= st.text_input(
-                "Stock Symbol",
-                placeholder="Stock ticker symbol",
+            symbol = st.text_input(
+                "Stock Symbol", placeholder="Stock ticker symbol", key="chart_symbol"
             ).upper()
         with col2:
             time_period = st.selectbox(
                 "Time Frame",
                 ["1d", "1w", "1mo", "3mo", "6mo", "1y", "5y"],
                 index=2,
+                key="chart_time_period",
             )
         with col3:
             time_interval = st.selectbox(
                 "Time Interval",
                 ["1m", "5m", "1d", "1w", "1mo"],
+                key="chart_time_interval",
             )
         with col4:
-            chart_type = st.selectbox("Chart Type", ["Candlestick", "Line"])
+            chart_type = st.selectbox(
+                "Chart Type", ["Candlestick", "Line"], key="chart_chart_type"
+            )
         with col5:
             indicators = st.multiselect(
                 "Indicators",
                 ["SMA_50", "EMA_20", "RSI", "MACD"],
-                max_selections=3,
+                max_selections=2,
+                key="chart_indicators",
             )
 
     if symbol and st.session_state.plot_type == "normal":
-        if st.session_state.fsa.is_valid_stock(symbol):
+        chart = plot_chart(
+            symbol=symbol,
+            period=time_period,
+            interval=time_interval,
+            plot_type="normal",
+            chart_type=chart_type,
+            indicators=indicators,
+        )
 
-            chart = plot_chart(
-                symbol=symbol,
-                period=time_period,
-                interval=time_interval,
-                plot_type="normal",
-                chart_type=chart_type,
-                indicators=indicators,
-            )
-
-            st.markdown(f"### Stock Chart for {symbol}")
-            st.plotly_chart(
-                chart,
-                use_container_width=True,
-            )
-        else:
-            st.toast(f"Stock ticker symbol {symbol} is not valid")
+        st.markdown(f"### Stock Chart for {symbol}")
+        st.plotly_chart(
+            chart,
+            use_container_width=True,
+        )
         st.session_state.plot_type = "none"
 
     elif symbol and st.session_state.plot_type == "prediction":
-        if symbol.split(".") in st.session_state.training_stocks:
-            chart = plot_chart(symbol=symbol, plot_type="prediction")
+        chart = plot_chart(symbol=symbol, plot_type="prediction")
 
-            st.markdown(f"### Predicted Stock Movement for {symbol}")
-            st.plotly_chart(chart, use_container_width=True)
-        else:
-            st.toast(f"Stock ticker symbol {symbol} not found in trained stocks")
+        st.markdown(f"### Predicted Stock Movement for {symbol}")
+        st.plotly_chart(chart, use_container_width=True)
         st.session_state.plot_type = "none"
 
     else:
@@ -97,16 +101,33 @@ with st.container(border=True):
     with st.container():
         with st_horizontal():
             if st.button("Plot", icon="📈"):
-                st.session_state.plot_type = "normal"
-                st.rerun()
+                if st.session_state.fsa.is_valid_stock(symbol):
+                    st.session_state.plot_type = "normal"
+                    st.rerun()
+                else:
+                    logging.error(
+                        f"Stock ticker symbol {symbol} was not found. {symbol} is either invalid or doesn't include the exchange identifiers."
+                    )
+                    st.toast(f"Stock ticker symbol {symbol} is not valid!", icon="⚠️")
 
-            if st.button("Predict", icon="🔮", disabled=True):
+            if st.button(
+                "Predict", icon="🔮", disabled=True, help="Under Construction"
+            ):
                 trained_stocks = st.session_state.get("training_stocks", [])
-                if not symbol in trained_stocks:
-                    st.toast("One or more selected stocks are not trained!", icon="⚠️")
+                if symbol.split(".") in st.session_state.trained_stocks:
+                    st.session_state.plot_type = "prediction"
+                    st.rerun()
+                else:
+                    logging.error(
+                        f"Stock ticker symbol {symbol} is not trained!. Train {symbol} in Daily Stock Page first!"
+                    )
+                    st.toast(
+                        f"Stock ticker symbol {symbol} is not trained!",
+                        icon="⚠️",
+                    )
 
-                st.session_state.plot_type = "prediction"
             if st.button("Reset", icon="❌"):
                 reset_selections()
             if st.button("Refresh", icon="🔃"):
                 st.cache_data.clear()
+                st.rerun()
