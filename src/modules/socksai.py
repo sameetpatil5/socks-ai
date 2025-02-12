d0ecff13d246f65ede962406198037c684d20457
Sameet Patil <sameetpatil5@gmail.com>
2025-02-12 15:28:35 +0530

    Add base imports in socksai.py to make interface module

--- src/modules/socksai.py
+++ src/modules/socksai.py
@@ -1,17 +1,5 @@
 from daily_stock_sentiment_agent import DailyStockSentimentAgent
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-dss_agent = DailyStockSentimentAgent(os.environ.get("MONGO_URI"), os.environ.get("MONGO_DB"))
-
-dss_agent.add_stock("ZOMATO.NS")
-
-# response = dss_agent.perform_quick_analysis()
-
-# dss_agent.fetch_stock_data()
-# dss_agent.fetch_stock_news()
-# print(response)
-# print(response.content)
-
+from find_stock_agent import FindStockAgent
+from stock_chart_agent import StockChartAgent
+from stock_chatbot_agent import StockChatbotAgent
+from stock_train_agent import StockTrainAgent
