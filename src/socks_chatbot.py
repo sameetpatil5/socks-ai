a16748e35894c024dbffe14e207daedf0dac5e47
Sameet Patil <sameetpatil5@gmail.com>
2025-02-14 21:37:21 +0530

    Add limit_resources function to avoid Gemini Resource exhausted error

--- src/socks_chatbot.py
+++ src/socks_chatbot.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 # Page config
 st.set_page_config(
@@ -8,6 +9,11 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+
+def limit_resources(seconds=10):
+    st.session_state.chat_input_disabled = True
+    time.sleep(seconds)
+    st.session_state.chat_input_disabled = False
 
 st.title("SocksAI Chatbot")
 
@@ -20,7 +26,7 @@ for message in st.session_state["messages"]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Talk to SocksAI"):
+if prompt := st.chat_input("Talk to SocksAI", placeholder="Talk to SocksAI...", disabled=st.session_state.get("chat_input_disabled", False)):
 
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -30,5 +36,7 @@ if prompt := st.chat_input("Talk to SocksAI"):
     with st.chat_message("assistant"):
         response = st.session_state.scba.chat(prompt)
         full_response = st.write_stream(response)
+
+    limit_resources()
 
     st.session_state["messages"].append({"role": "assistant", "content": full_response})
