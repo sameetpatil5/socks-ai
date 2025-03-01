4003c2b621f7e98e8291926bb9f430af8205066a
Sameet Patil <sameetpatil5@gmail.com>
2025-03-01 15:56:25 +0530

    Refactor and Optimize code
    1. Added a `model` argument,
    requiring the Gemini model to be passed during object initialization.
    2. Added exception handling at critical points.
    3. Refactor `add_stock` -> `find_stock`.
    4. Improve `find_stock_agent` Agent prompt.

--- src/modules/find_stock_agent.py
+++ src/modules/find_stock_agent.py
@@ -9,47 +9,68 @@ import ast
 from .models.models import StockSymbol
 
 # Configure logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
+logger = logging.getLogger("app")
 
 
 class FindStockAgent:
     """
     A class to find and validate stock ticker symbols based on user queries.
 
-    This class uses an AI agent to identify stock symbols and validate them
-    using the yfinance library.
+    Attributes:
+        model (Gemini): The Gemini model used by the AI agent.
+        stock_agent (Agent): The AI agent used to find stock symbols.
     """
 
-    def __init__(self):
+    def __init__(self, model: Gemini):
         """
-        Initialize the FindStockAgent with a stock agent configuration.
+        Initializes the FindStockAgent with the given model.
+
+        Args:
+            model (Gemini): The Gemini model to use for the AI agent.
+
         """
-        self.stock = None
+        # Model setup
+        try:
+            self.model = model
+            logger.info("Model Loaded")
+        except Exception as e:
+            logger.error(f"Error while loading model: {e}")
 
-        # Stock Agent configuration
-        self.stock_agent = Agent(
-            name="Stock Symbol Finder",
-            role="Identify the exact stock ticker symbol based on the user's query.",
-            description="This Agent will search for the exact stock ticker symbol based on the user's query.",
-            response_model=StockSymbol,
-            model=Gemini(id="gemini-2.0-flash-exp"),
-            tools=[YFinanceTools(), GoogleSearch()],
-            instructions=[
-                "Your task is to search for the exact stock ticker symbol based on the user's query.",
-                "Do not include any descriptions, explanations, or additional details in the output.",
-                "If a single stock ticker is found, return only the ticker symbol as a plain string inside a list.",
-                "If multiple stock tickers are found, return them as a list of plain strings.",
-                "If no stock ticker is found, return an empty list.",
-                "Avoid providing any additional commentary, context, or information in your response.",
-            ],
-            markdown=False,
-            structured_outputs=True,
-            tool_choice="auto",
-        )
+        # Stock Agent
+        try:
+            self.stock_agent = Agent(
+                name="Stock Symbol Finder",
+                role="Stock Ticker Identifier",
+                description="Finds the exact stock ticker symbol based on the user's query.",
+                response_model=StockSymbol,
+                model=self.model,
+                tools=[YFinanceTools(), GoogleSearch()],
+                instructions=[
+                    "Identify the correct stock ticker symbol based on the user's query.",
+                    "Search on Google for the appropriate stock ticker symbol.",
+                    "Validate the stock ticker symbol using yfinance.",
+                    "**Response Guidelines:**",
+                    "- If a single ticker is found, return it as a plain string inside a list.",
+                    "- If multiple tickers are found, return them as a list of plain strings.",
+                    "- If no ticker is found, return an empty list.",
+                    "**Strict Constraints:**",
+                    "- Do **not** include descriptions, explanations, or additional details.",
+                    "- Do **not** provide any context, commentary, or extra information.",
+                    "- Keep the response strictly formatted as per the schema.",
+                    "**Output Requirements:**",
+                    "- `stock_symbols` (List): A list of stock ticker symbols.",
+                    "  - Each item in the list follows the structure:",
+                    "    - `symbol` (str): The stock ticker symbol.",
+                ],
+                markdown=False,
+                structured_outputs=True,
+                tool_choice="auto",
+            )
+            logger.info("Stock Agent Loaded")
+        except Exception as e:
+            logger.error("Error while loading Stock Agent")
 
-    def get_stock(self, query: str) -> str:
+    def get_stock(self, query: str) -> List:
         """
         Use the stock agent to find stock ticker symbols based on a user query.
 
@@ -57,14 +78,20 @@ class FindStockAgent:
         query (str): The user's query to search for stock ticker symbols.
 
         Returns:
-        str: The stock ticker symbol(s) as a plain string or an empty list if none found.
+        List[str]: List of valid stock ticker symbol(s) appropriate as per the query.
         """
-        logging.info(f"Querying stock symbols for: {query}")
-        stock = self.stock_agent.run(
-            f"Find the stock ticker symbol(s) for the following query: '{query}'.\n"
-            "Only return the ticker symbol(s)."
-        )
-        return stock.content
+        stocks = []
+        try:
+            logger.info(f"Querying stock symbols for: {query}")
+            response = self.stock_agent.run(
+                f"Find the stock ticker symbol(s) for the following query: '{query}'.\n"
+                "Only return the ticker symbol(s)."
+            )
+            stocks = response.content
+            logger.info(f"Querying was successful with {len(stocks)}, {stocks} found")
+        except Exception as e:
+            logger.error(f"Error while Querying[{query}]: {e}")
+        return stocks
 
     def is_valid_stock(self, symbol: str) -> bool:
         """
@@ -76,39 +103,51 @@ class FindStockAgent:
         Returns:
         bool: True if the symbol is valid, False otherwise.
         """
-        logging.info(f"Validating stock symbol: {symbol}")
-        ticker = yf.Ticker(symbol)
-        return (
-            True
-            if "symbol" in ticker.info and ticker.info["symbol"] == symbol
-            else False
-        )
+        try:
+            logger.info(f"Validating stock symbol: {symbol}")
+            ticker = yf.Ticker(symbol)
+            if ticker is not None:
+                return (
+                    True
+                    if "symbol" in ticker.info and ticker.info["symbol"] == symbol
+                    else False
+                )
+            else:
+                logger.warning("Invalid stock symbol. Will run a Query...")
+                return False            
+        except Exception as e:
+            logger.error(f"Error while validing the stock symbol[{symbol}]: {e}")
 
-    def add_stock(self, query: str) -> List[str]:
+    def find_stock(self, query: str) -> List[str]:
         """
-        Add a stock symbol to the list if valid, or find and return potential symbols.
+        Find stock ticker symbols based on a user query.
 
         Args:
-        query (str): The stock symbol or user query to process.
+            query (str): The user's query to search for stock ticker symbols.
 
         Returns:
-        List[str]: A list of valid stock symbols.
+            List[str]: List of valid stock ticker symbol(s) appropriate as per the query.
         """
-        logging.info(f"Processing stock query: {query}")
+        try:
+            logger.info(f"Processing stock query: {query}")
 
-        if self.is_valid_stock(query):
-            logging.info(f"Stock symbol '{query}' is valid.")
-            return [query.upper()]
-        else:
-            logging.info(
-                f"Stock symbol '{query}' is not valid. Searching for alternatives."
-            )
-            stock_symbols = self.get_stock(query)
-            try:
-                # Parse the returned stock symbols into a list
-                list_symbols = ast.literal_eval(stock_symbols.strip().splitlines()[0])
-                logging.info(f"Found stock symbols: {list_symbols}")
-                return list_symbols
-            except (SyntaxError, ValueError) as e:
-                logging.error(f"Error parsing stock symbols: {e}")
-                return []
+            if self.is_valid_stock(query):
+                logger.info(f"Stock symbol '{query}' is valid.")
+                return [query.upper()]
+            else:
+                logger.info(
+                    f"Stock symbol '{query}' is not valid. Searching the stock with Stock Agent."
+                )
+                stock_symbols = self.get_stock(query)
+                try:
+                    list_symbols = ast.literal_eval(
+                        stock_symbols.strip().splitlines()[0]
+                    )
+                    logger.info(f"Found stock symbols: {list_symbols}")
+                    return list_symbols
+                except (SyntaxError, ValueError) as e:
+                    logger.error(f"Error parsing stock symbols: {e}")
+                    return []
+        except Exception as e:
+            logger.error("Error while Processing the stock")
+            return []
